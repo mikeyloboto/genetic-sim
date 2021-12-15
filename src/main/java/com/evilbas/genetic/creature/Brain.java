20ae3f3ef16024d5c12eb288634bf0a39228b128
Evilbas <dimonloboto@gmail.com>
2021-12-15 09:50:33 -0500

    Neural connections

--- src/main/java/com/evilbas/genetic/creature/Brain.java
+++ src/main/java/com/evilbas/genetic/creature/Brain.java
@@ -1,12 +1,15 @@
 package com.evilbas.genetic.creature;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Data;
 
 @Data
 public class Brain {
-    private static final Integer MAX_CONNECTIONS = 20;
+    private static final Integer MAX_CONNECTIONS = 100;
+    private static final Integer INIT_CONNECTIONS = 10;
+    private static final Integer MID_LAYER = 10;
     // Inputs
     private Neuron inputEast;
     private Neuron inputWest;
@@ -15,7 +18,6 @@ public class Brain {
 
     private Neuron inputRandom;
     private Neuron inputHunger;
-    private Neuron inputAge;
 
     // Hidden
     private List<Neuron> hiddenNeurons;
@@ -28,6 +30,65 @@ public class Brain {
     private Neuron outputWest;
     private Neuron outputEat;
 
+    // Collections
+
+    private List<Neuron> viableInputs;
+    private List<Neuron> viableOutputs;
+
+    private List<Connection> connections;
+
+    private Brain() {
+
+        this.viableInputs = new ArrayList<>();
+        this.viableOutputs = new ArrayList<>();
+        this.hiddenNeurons = new ArrayList<>();
+
+        this.inputEast = Neuron.builder().type(NeuronType.INPUT).build();
+        this.inputWest = Neuron.builder().type(NeuronType.INPUT).build();
+        this.inputNorth = Neuron.builder().type(NeuronType.INPUT).build();
+        this.inputSouth = Neuron.builder().type(NeuronType.INPUT).build();
+        this.inputRandom = Neuron.builder().type(NeuronType.INPUT).build();
+        this.inputHunger = Neuron.builder().type(NeuronType.INPUT).build();
+
+        this.viableInputs.add(inputEast);
+        this.viableInputs.add(inputWest);
+        this.viableInputs.add(inputNorth);
+        this.viableInputs.add(inputSouth);
+        this.viableInputs.add(inputRandom);
+        this.viableInputs.add(inputHunger);
+
+        for (int i = 0; i < MID_LAYER; i++) {
+            Neuron mid = Neuron.builder().type(NeuronType.HIDDEN).build();
+            this.hiddenNeurons.add(mid);
+            this.viableInputs.add(mid);
+            this.viableOutputs.add(mid);
+        }
+
+        this.outputEast = Neuron.builder().type(NeuronType.OUTPUT).build();
+        this.outputWest = Neuron.builder().type(NeuronType.OUTPUT).build();
+        this.outputNorth = Neuron.builder().type(NeuronType.OUTPUT).build();
+        this.outputSouth = Neuron.builder().type(NeuronType.OUTPUT).build();
+        this.outputEat = Neuron.builder().type(NeuronType.OUTPUT).build();
+
+        this.viableOutputs.add(outputEast);
+        this.viableOutputs.add(outputWest);
+        this.viableOutputs.add(outputNorth);
+        this.viableOutputs.add(outputSouth);
+        this.viableOutputs.add(outputEat);
+
+        this.connections = new ArrayList<>();
+
+    }
+
+    public void processStep() {
+        // Clear
+        for (Neuron n : this.viableOutputs) {
+            n.setValue(0);
+        }
+
+        // Calc
+    }
+
     public static Brain mutate(Brain origin) {
         try {
             return (Brain) origin.clone();
@@ -38,6 +99,18 @@ public class Brain {
     }
 
     public static Brain generateRandom() {
-        return null;
+        Brain result = new Brain();
+
+        return result;
     }
+
+    public Boolean duplicateConnection(Connection check) {
+        for (Connection c : connections) {
+            if (c.getInput() == check.getInput() && c.getOutput() == check.getOutput()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/main/java/com/evilbas/genetic/creature/Connection.java
+++ src/main/java/com/evilbas/genetic/creature/Connection.java
@@ -3,12 +3,29 @@ package com.evilbas.genetic.creature;
 import lombok.Data;
 
 @Data
-public class Connection {
+public class Connection implements Comparable<Connection> {
     private Neuron input;
     private Neuron output;
     private Integer mod;
 
     public void procConnection() {
-        this.getOutput().setValue(this.getInput().getValue() * this.getMod());
+        this.getOutput().setValue(this.getOutput().getValue() + (this.getInput().getValue() * this.getMod()));
+    }
+
+    public Integer compareValue() {
+        if (this.getInput().getType() == NeuronType.INPUT && this.getOutput().getType() == NeuronType.OUTPUT)
+            return 0;
+        if (this.getInput().getType() == NeuronType.INPUT && this.getOutput().getType() == NeuronType.HIDDEN)
+            return 1;
+        if (this.getInput().getType() == NeuronType.HIDDEN && this.getOutput().getType() == NeuronType.HIDDEN)
+            return 2;
+        if (this.getInput().getType() == NeuronType.HIDDEN && this.getOutput().getType() == NeuronType.OUTPUT)
+            return 3;
+        return 4;
+    }
+
+    @Override
+    public int compareTo(Connection o) {
+        return o.compareValue() - this.compareValue();
     }
 }
