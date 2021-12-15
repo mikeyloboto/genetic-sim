e85f8b447352b4c0e23697d3886a8371bc3542dd
Evilbas <dimonloboto@gmail.com>
2021-12-15 04:53:32 -0500

    Init

---
+++ src/main/java/com/evilbas/genetic/Launcher.java
@@ -0,0 +1,11 @@
+package com.evilbas.genetic;
+
+/**
+ * Hello world!
+ *
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
